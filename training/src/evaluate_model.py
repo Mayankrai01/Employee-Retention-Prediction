5123f374be4657de87478f8fff31af1f5978536b
Mayank <mayank.101120@gmail.com>
2023-08-24 10:34:30 +0530

    first commit

--- training/src/evaluate_model.py
+++ training/src/evaluate_model.py
@@ -11,6 +11,9 @@ from hydra.utils import to_absolute_path as abspath
 from omegaconf import DictConfig
 from sklearn.metrics import accuracy_score, f1_score
 from xgboost import XGBClassifier
+from mlflow.models import infer_signature
+from urllib.parse import urlparse
+
 
 logger = BaseLogger()
 
@@ -18,7 +21,8 @@ logger = BaseLogger()
 def load_data(path: DictConfig):
     X_test = pd.read_csv(abspath(path.X_test.path))
     y_test = pd.read_csv(abspath(path.y_test.path))
-    return X_test, y_test
+    X_train = pd.read_csv(abspath(path.X_train.path))
+    return X_train,X_test, y_test
 
 
 def load_model(model_path: str):
@@ -50,12 +54,13 @@ def evaluate(config: DictConfig):
     with mlflow.start_run():
 
         # Load data and model
-        X_test, y_test = load_data(config.processed)
+        X_train,X_test, y_test = load_data(config.processed)
 
         model = load_model(abspath(config.model.path))
 
         # Get predictions
         prediction = predict(model, X_test)
+        signature = infer_signature(X_train, prediction)
 
         # Get metrics
         f1 = f1_score(y_test, prediction)
@@ -64,7 +69,12 @@ def evaluate(config: DictConfig):
         accuracy = accuracy_score(y_test, prediction)
         print(f"Accuracy Score of this model is {accuracy}.")
 
-        # Log metrics
+        # Loggin
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+        if tracking_url_type_store != "file":
+            mlflow.sklearn.log_model(XGBClassifier, "model", registered_model_name="xgboost", signature=signature)
+        else:
+            mlflow.sklearn.log_model(XGBClassifier, "model", signature=signature)
         log_params(model, config.process.features)
         log_metrics(f1_score=f1, accuracy_score=accuracy)
 
